77a3610bf1b4d7caaa665449ed1a567c2bdad2c5
Shyngys03 <s_zhekebaev@kbtu.kz>
2021-02-09 17:44:02 +0600

    TSIS 1

--- TSIS 1/типы данных/Символы и строки/107.py
+++ TSIS 1/типы данных/Символы и строки/107.py
@@ -1,17 +1,20 @@
 s = input()
 
-t = ""
-maxi = ""
+t = str()
+maxi = str()
 cnt = 0
 mx = 0
 
 for i in s:
     if i != ' ':
-        t += i
-        maxi += i
+        t = t.join(i)
         cnt += 1
     
     elif i == ' ':
         if mx < cnt:
             mx = cnt
-            
+            maxi = t
+        cnt = 0
+        t = 0
+
+print(maxi, '\n', mx)
